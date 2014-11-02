b490593f46a9a7bba8ee9d4b04fe57adaf16ba69
maadlog <martin.loguancio@gmail.com>
2014-11-02 20:09:01 -0300

    Revert "Revert "ABMRol y forms de AltaRol agregados""
    This reverts commit eddba863fcd244f68d2a7903e9845e240c01fc85.

---
+++ FrbaHotel/FrbaHotel/ABM de Rol/ABMRol.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaHotel.ABM_de_Rol
+{
+    public partial class ABMRol : ABM
+    {
+        public ABMRol()
+        {
+            InitializeComponent();
+        }
+
+        public ABMRol(NavegableForm owner)
+            : base(owner, "un", "Rol")
+        {
+            InitializeComponent();
+        }
+
+        private void ABMRol_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public override void alta()
+        {
+            new AltaRol(this).FinalStandaloneOpen();
+        }
+
+
+
+    }
+}
