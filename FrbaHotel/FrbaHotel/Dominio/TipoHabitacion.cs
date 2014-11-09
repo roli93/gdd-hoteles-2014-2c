8647c517d32ca4b497602b01fa8ddadbbf6b813b
Rodolfo Caputo <rodcap.93@gmail.com>
2014-11-09 18:32:06 -0300

    Intento agregar algunos ignores

--- FrbaHotel/FrbaHotel/Dominio/TipoHabitacion.cs
+++ FrbaHotel/FrbaHotel/Dominio/TipoHabitacion.cs
@@ -7,27 +7,19 @@ namespace FrbaHotel.Dominio
 {
     public class TipoHabitacion : Entidad
     {
-
-<<<<<<< HEAD
-=======
         public string descripcion;
 
->>>>>>> 4ff1457ea665f6287566306f0e9edcf07a27a5f1
         public TipoHabitacion(int unId,string unaDescripcion)
         {
             this.Id = unId;
             this.descripcion = unaDescripcion;
         }
 
-<<<<<<< HEAD
         public string Descripcion { get; set; }
 
         public override string ToString()
         {
             return Descripcion;
         }
-
-=======
->>>>>>> 4ff1457ea665f6287566306f0e9edcf07a27a5f1
     }
 }
