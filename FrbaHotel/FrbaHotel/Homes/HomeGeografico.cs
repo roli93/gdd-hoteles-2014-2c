e567e69be7292d5c0f579e60d69f8ac26e5d609f
maadlog <martin.loguancio@gmail.com>
2014-11-11 00:15:23 -0300

    Borrados esos mismos 2 sp porque estaban repetidos

--- FrbaHotel/FrbaHotel/Homes/HomeGeografico.cs
+++ FrbaHotel/FrbaHotel/Homes/HomeGeografico.cs
@@ -2,10 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using FrbaHotel.Administracion_Base_de_Datos;
 
 namespace FrbaHotel.Homes
 {
-    class HomeGeografico
+    class HomeGeografico : Home
     {
+        public static DataTable buscarPaises()
+        {/*TODO
+           
+            return DatabaseAdapter.traerDataTable("paises").;
+            */
+            DataTable ej = new DataTable();
+            ej.Clear(); 
+            ej.Columns.Add("ID");
+            ej.Columns.Add("Descripcion");
+         
+            ej.Rows.Add(new object[] { 1,"Argentina"});
+            ej.Rows.Add(new object[] { 2, "Brasil" });
+            return ej;
+
+        }
+
+        public static DataTable buscarCiudadesDe(int unIdPais)
+        {
+            /*TODO
+           
+            return DatabaseAdapter.traerDataTable("ciudades_de_pais",unIdPais);
+            */
+            DataTable ej = new DataTable();
+            ej.Clear();
+            ej.Columns.Add("ID");
+            ej.Columns.Add("Descripcion");
+
+            ej.Rows.Add(new object[] { 1, "Argentina" });
+            ej.Rows.Add(new object[] { 2, "Brasil" });
+            return ej;
+
+
+        }
+
     }
 }
