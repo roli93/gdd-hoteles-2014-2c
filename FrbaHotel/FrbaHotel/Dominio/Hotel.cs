9bf073b2377de58f2109f89e66c6d26741546a9a
maadlog <martin.loguancio@gmail.com>
2014-11-11 00:09:51 -0300

    Agregado ultimos 2 sp que me comi para los paises

--- FrbaHotel/FrbaHotel/Dominio/Hotel.cs
+++ FrbaHotel/FrbaHotel/Dominio/Hotel.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using FrbaHotel.Dominio;
 using FrbaHotel.Administracion_Base_de_Datos;
 
 namespace FrbaHotel
@@ -18,7 +19,17 @@ namespace FrbaHotel
             this.Id = id;
             this.Nombre = nombre;
         }
+
         public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public int Estrellas { get; set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public List<Regimen> Regimenes { get; set; }
+        public string FechaCreacion { get; set; }
+
 
         public override string ToString()
         {
